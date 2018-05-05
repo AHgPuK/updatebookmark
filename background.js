6430236887142cbd9095a50eed1d7b92339359f0
Andrey Hohutkin <Andrey Hohutkin@localhost>
2018-05-05 13:24:20 +0300

    Add shortcut to bookmark context menu

---
+++ background.js
@@ -0,0 +1,101 @@
+const MENU_ENTRY = {
+	MAIN_MENU: 'MAIN_MENU',
+	UPDATE_TITLE: 'UPDATE_TITLE',
+	UPDATE_URL: 'UPDATE_URL',
+	UPDATE_TITLE_URL: 'UPDATE_TITLE_URL',
+}
+
+browser.contextMenus.create({
+	id: MENU_ENTRY.MAIN_MENU,
+	title: "Update bookmark",
+	contexts: ["bookmark"],
+	type: 'normal',
+	icons: {
+		'32': 'icons/icon32.png'
+	}
+});
+
+browser.contextMenus.create({
+	id: MENU_ENTRY.UPDATE_URL,
+	parentId: MENU_ENTRY.MAIN_MENU,
+	title: "Update URL",
+	contexts: ["bookmark"],
+	type: 'normal'
+});
+
+browser.contextMenus.create({
+	id: MENU_ENTRY.UPDATE_TITLE_URL,
+	parentId: MENU_ENTRY.MAIN_MENU,
+	title: "Update URL/Title",
+	contexts: ["bookmark"],
+	type: 'normal'
+});
+
+browser.contextMenus.create({
+	id: MENU_ENTRY.UPDATE_TITLE,
+	parentId: MENU_ENTRY.MAIN_MENU,
+	title: "Update Title",
+	contexts: ["bookmark"],
+	type: 'normal'
+});
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+
+	if (info.menuItemId === MENU_ENTRY.UPDATE_URL)
+	{
+		bookmarkSelected(info.bookmarkId, true, false);
+	}
+	else if (info.menuItemId === MENU_ENTRY.UPDATE_TITLE_URL)
+	{
+		bookmarkSelected(info.bookmarkId, true, true);
+	}
+	else if (info.menuItemId === MENU_ENTRY.UPDATE_TITLE)
+	{
+		bookmarkSelected(info.bookmarkId, false, true);
+	}
+
+});
+
+function bookmarkSelected(bookmarkId, isUrl, isTitle) {
+
+	browser.tabs.query({active: true, currentWindow: true})
+	.then(function (tabs) {
+
+		if (!(tabs && tabs[0]))
+		{
+			return Promise.reject(0);
+		}
+
+		var tab = tabs[0];
+
+		if ((tab.title && tab.url) || tab.status == 'complete')
+		{
+
+		}
+		else
+		{
+			return Promise.reject(0);
+		}
+
+		var update = {};
+
+		if (isUrl)
+		{
+			update.url = tab.url;
+		}
+
+		if (isTitle)
+		{
+			update.title = tab.title;
+		}
+
+		return browser.bookmarks.update(bookmarkId, update);
+	})
+	.then(function () {
+
+	})
+	.catch(function (err) {
+		return Promise.resolve();
+	})
+
+}
